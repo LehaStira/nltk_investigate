a1f9dd85411d261fd1d454bd6bd2861d5297161d
Alexey Kolesnik <alexeyykolesnik@gmail.com>
2021-06-03 01:21:44 +0300

    All right, we can use TOKENIZE for some strings, and we can use it in nltk sense, i got it

---
+++ Practical_bigrams.py
@@ -0,0 +1,15 @@
+import nltk
+
+word_data = "The best performance can bring in sky high success."
+nltk_tokens = nltk.word_tokenize(word_data)
+print(nltk_tokens)
+print(type(nltk_tokens))
+
+print(list(nltk.bigrams(nltk_tokens)))
+
+example_list = ['Hello', 'I', 'Love', 'You', 'son', 'of', 'a', 'bitch']
+
+list_token = nltk.word_tokenize()
+
+print(list(nltk.bigrams(example_list)))
+print(list(nltk.bigrams(list_token)))
