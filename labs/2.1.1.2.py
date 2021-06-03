37ca554d2633a5e7bfdcbe786fe8d4dc35b879f3
leha_stira <leha_stira@ukr.net>
2021-06-03 13:38:45 +0300

    2.1.1.2 had solved

---
+++ labs/2.1.1.2.py
@@ -0,0 +1,22 @@
+from nltk.corpus import nps_chat
+from nltk import FreqDist
+
+
+def main():
+    chatroom = nps_chat.posts('10-19-20s_706posts.xml')
+    fdist = FreqDist(str(chatroom))
+    all_chat = list(chatroom)
+    result = list()
+    for i in all_chat:
+        for j in i:
+            result.append(j)
+    print(result)
+    result = [i for i in result if not i.islower()]
+    result = [i for i in result if i != '.' and i != ',' and i != '.' and i != '/' and i != "'" and i != '!']
+    result = ' '.join(result)
+    fdist = FreqDist(str(result))
+    print(fdist.most_common(10))
+
+
+if __name__ == '__main__':
+    main()
